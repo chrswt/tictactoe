cec25507e85ec32251aa451d38eb67f52ab65a58
Christopher Tham <chriswt91@gmail.com>
2016-12-05 15:00:04 -0800

    Implement checkWin method

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ Game.prototype = {
 
   checkWinHorizontal: function() {
     this.board.forEach(function(row) {
-      if (row[0] === row[1] === row[2]) {
+      if (row[0] === row[1] && row[1] === row[2]) {
         return row[0] === '-' ? null :
           row[0] === 'X' ? 'X wins' :
           'O wins';
@@ -24,25 +24,31 @@ Game.prototype = {
   },
 
   checkWinVertical: function() {
-    this.board.forEach(function(col, idx) {
-      if (this.board[idx][0] === this.board[idx][1] === this.board[idx][2]) {
+    for (var idx = 0; idx <= 2; idx++) {
+      if (this.board[idx][0] === this.board[idx][1] && 
+        this.board[idx][1] === this.board[idx][2]) {
         return this.board[idx][0] === '-' ? null :
           this.board[idx][0] === 'X' ? 'X wins' :
           'O wins';
       }
-    });
+    }
   },
 
   checkWinDiagonal: function() {
-    if (this.board[0][0] === this.board[1][1] === this.board[2][2]) {
+    if (this.board[0][0] === this.board[1][1] && 
+      this.board[1][1] === this.board[2][2]) {
       return this.board[0][0] === '-' ? null :
         this.board[0][0] === 'X' ? 'X wins' :
         'O wins';
-    } else if (this.board[0][2] === this.board[1][1] === this.board[2][0]) {
+    } else if (this.board[0][2] === this.board[1][1] 
+      && this.board[1][1]=== this.board[2][0]) {
       return this.board[0][2] === '-' ? null :
         this.board[0][2] === 'X' ? 'X wins' :
         'O wins';
     }
   },
-};
 
+  checkWin: function() {
+    return this.checkWinHorizontal() || this.checkWinVertical() || this.checkWinDiagonal();
+  }
+};
