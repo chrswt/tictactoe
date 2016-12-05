a941043715068281455126ceb0ff3a86fea36671
Christopher Tham <chriswt91@gmail.com>
2016-12-05 14:46:15 -0800

    Implement checkWinHorizontal method

--- main.js
+++ main.js
@@ -11,5 +11,18 @@ Game.prototype = {
     this.board.forEach(function(row) {
       console.log(row);
     });
-  }
+  },
+
+  checkWinHorizontal: function() {
+    this.board.forEach(function(row) {
+      if (row[0] === row[1] === row[2]) {
+        return row[0] === '-' ? null :
+          row[0] === 'X' ? 'X wins' :
+          'O wins';
+      }
+    })
+  },
+
+  
 };
+
