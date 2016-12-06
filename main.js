21fb3b18a86630530dba24f66668713b671ecf18
Christopher Tham <chriswt91@gmail.com>
2016-12-05 16:56:30 -0800

    Fix win checking and gameplay

--- main.js
+++ main.js
@@ -17,19 +17,20 @@ Game.prototype = {
   },
 
   checkWinHorizontal: function() {
-    this.board.forEach(function(row) {
+    for (var idx = 0; idx <= 2; idx++) {
+      var row = this.board[idx];
       if (row[0] === row[1] && row[1] === row[2]) {
         return row[0] === '-' ? null :
           row[0] === 'X' ? 'X wins' :
           'O wins';
       }
-    })
+    }
   },
 
   checkWinVertical: function() {
     for (var idx = 0; idx <= 2; idx++) {
-      if (this.board[idx][0] === this.board[idx][1] && 
-        this.board[idx][1] === this.board[idx][2]) {
+      if (this.board[0][idx] === this.board[1][idx] && 
+        this.board[1][idx] === this.board[2][idx]) {
         return this.board[idx][0] === '-' ? null :
           this.board[idx][0] === 'X' ? 'X wins' :
           'O wins';
@@ -75,27 +76,44 @@ Game.prototype = {
     while (!this.gameOver) {
       if (this.checkWin()) {
         this.gameOver = true;
+        break;
       } else {
         this.printBoard();
-        console.log('oi get here');
         this.promptPlayer1();
-        if (this.checkWin()) { console.log('X wins!'); }
+        if (this.checkWin()) { 
+          console.log('X wins!');
+          console.log('Final game board');
+          console.log(this.printBoard());
+          this.gameOver = true;
+          break;
+        }
         this.printBoard();
         this.promptPlayer2();
-        if (this.checkWin()) { console.log('O wins!'); }
+        if (this.checkWin()) { 
+          console.log('O wins!');
+          console.log('Final game board');
+          console.log(this.printBoard());
+          this.gameOver = true;
+          break;
+        }
       }
     }
-
-    return this.gameOver; // TODO
   },
 
   promptPlayer1: function() {
     var pos = readlineSync.question('Player 1: You are [X], where would you like to place your piece? Input accepted: row, col');
+
+    if (pos.split(',').length !== 2) {
+      console.log('Please enter input "row, col"!');
+      this.promptPlayer1();
+    }
+
     var row = pos.split(',')[0];
     var col = pos.split(',')[1];
 
     if (row >= 3 || row < 0 || col >= 3 || col < 0) {
       console.log('Invalid input, please specify a row/col between 0 and 2');
+      this.promptPlayer1();
     } else {
       this.board[row][col] = 'X';
       console.log('Successfully placed your piece!');
@@ -104,11 +122,18 @@ Game.prototype = {
 
   promptPlayer2: function() {
     var pos = readlineSync.question('Player 2: You are [O], where would you like to place your piece? Input accepted: row, col');
+
+    if (pos.split(',').length !== 2) {
+      console.log('Please enter input "row, col"!');
+      this.promptPlayer2();
+    }
+
     var row = pos.split(',')[0];
     var col = pos.split(',')[1];
 
     if (row >= 3 || row < 0 || col >= 3 || col < 0) {
       console.log('Invalid input, please specify a row/col between 0 and 2');
+      this.promptPlayer2();
     } else {
       this.board[row][col] = 'O';
       console.log('Successfully placed your piece!');
